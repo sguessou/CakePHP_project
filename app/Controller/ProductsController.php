f1da5a675f70b2842e66db24c13558d4a55b3c8b
Saad Guessous <guessous_saad@hotmail.com>
2013-09-26 17:22:03 +0300

    Fixed search query.

--- app/Controller/ProductsController.php
+++ app/Controller/ProductsController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class ProductsController extends AppController {
 
 	public $helpers = array('Html', 'Form');
@@ -7,7 +8,30 @@ class ProductsController extends AppController {
 	public function index()
 	{
 		$this->set('title_for_layout', 'Verkkokauppa');
+        
+        $this->loadModel('Product_type');
+
+        $this->set('ptypes', $this->Product_type->find('all'));
 	}
+
+    public function search()
+    {
+        
+        if ( ! empty($this->data))
+        {
+            $products = $this->Product->find('all', 
+                array('conditions' => 
+                              array('Product.product_name LIKE' => '%'.strtoupper($this->data['Product']['product_name']).'%',
+                                    'Product.ptype_id' => (int) $this->data['Product']['ptype_id'])
+                                           ));
+
+            $this->set('products', $products);
+        }
+        elseif ( empty($this->data)) 
+        {
+            $this->set('products', 'null');
+        }          
+    }
 
 	
 
