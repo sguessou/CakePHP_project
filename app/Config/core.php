6f737c37bcc8f55d2264fcc837a5bad9d428f707
Saad Guessous <guessous_saad@hotmail.com>
2013-09-29 14:21:56 +0300

    Experimenting with DebugKit.

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,13 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '520tI82X1x56Zd9Sf3z6xItv476fHIvk');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '77772930919493878148157900113');
+
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
