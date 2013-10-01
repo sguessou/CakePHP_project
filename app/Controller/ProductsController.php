57734a6e21edd75f9616f9a49a27f657c25810d9
Saad Guessous <guessous_saad@hotmail.com>
2013-10-01 16:52:43 +0300

    Created new method test.

--- app/Controller/ProductsController.php
+++ app/Controller/ProductsController.php
@@ -225,5 +225,51 @@ class ProductsController extends AppController {
         return;
     }
 
+    public function test()
+    {
+        $db = ConnectionManager::getDataSource('default');
+
+        $this->logUser('Index');
+
+        $this->set('title_for_layout', 'Verkkokauppa');
+        
+        $this->loadModel('Product_type');
+        
+        $this->set('ptypes', $this->Product_type->find('all'));
+
+        // If cart_id is in the session, get it from there 
+        $cart_id = $this->Session->read('cartId');
+
+        // If not we generate a new one and save it in the session
+        if (! $cart_id)
+        {
+            $cart_id = md5( uniqid(rand(), true) );
+
+            $this->Session->write('cartId', $cart_id);
+        }
+
+        if ( $this->RequestHandler->isAjax())
+        {
+            $cartId = $this->Session->read('cartId');
+
+            $this->logUser('IndexAjax');
+                
+            $this->set('dataitems', $db->fetchAll('SELECT products.* , product_types.type_name as typeName FROM products 
+                                                   INNER JOIN cartitems
+                                                   INNER JOIN product_types  
+                                                   WHERE products.product_id = cartitems.product_id
+                                                   AND products.ptype_id = product_types.ptype_id 
+                                                   AND cartitems.cart_id LIKE ?', array($cartId)));   
+
+            $this->render('add_to_cart', 'ajax');
+        }   
+         
+        $count = $db->fetchAll('SELECT COUNT(*) as cnt FROM cartitems WHERE cart_id LIKE ?', array($cart_id));
+
+        $this->set('count', (int) $count[0][0]['cnt']);
+
+        $this->pageTitle = 'Verkkokauppa';
+    }
+
 	
 }
