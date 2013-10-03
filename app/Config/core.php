774b301f1e814d95b39a2e992a3ffdcff8b9f19f
Saad Guessous <guessous_saad@hotmail.com>
2013-10-03 13:41:45 +0300

    Fiddled with Session values.

--- app/Config/core.php
+++ app/Config/core.php
@@ -219,8 +219,8 @@
  */
 	Configure::write('Session', array(
 		'defaults' => 'php',
-		'timeout' => 180, // The session will timeout after 30 minutes of inactivity
-        'cookieTimeout' => 1440, // The session cookie will live for at most 24 hours, this does not effect session timeouts
+		'timeout' => 4320, // The session will timeout after 30 minutes of inactivity
+        'cookieTimeout' => 1440, //The session cookie will live for at most 24 hours, this does not effect session timeouts
         'checkAgent' => false,
         'autoRegenerate' => true // causes the session expiration time to reset on each page load
 	));
